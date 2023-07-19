a1e31c8e57db32f38564fca45a88bfa0d1b21946
Andrew Trotter <andrewjtrotter@gmail.com>
2023-07-19 16:12:41 +1200

    added favicon and removed self desc

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
           <h1 className="text-5xl mb-2 font-bold">Andrew Trotter</h1>
           <h2 className="text-2xl font-medium mb-2">Full Stack Web Developer</h2>
         </hgroup>
-        <p className="text-gray-400 w-3/4 mr-9">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et lobortis eros.</p>
+        <p className="text-gray-400 w-3/4 mr-9"></p>
       </div>
       <Nav />
       
