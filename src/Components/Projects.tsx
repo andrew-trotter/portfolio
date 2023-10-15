f377c098eee17b5e12e18e33458d915338ef8dfa
Andrew Trotter <andrewjtrotter@gmail.com>
2023-10-16 10:43:43 +1300

    Added wip logic and what's next area

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,6 +4,7 @@ import { Project } from '../../public/models/project'
 
 function Projects() {
   const data = projects
+  let wip: Project
   return (
     <div id="projects">
       <h1 className="text-gray-400 font-sans text-2xl mb-4">
@@ -11,11 +12,18 @@ function Projects() {
       </h1>
 
       <ul className="mb-24">
-        {data.map((project: Project) => (
-          project.display &&
+        {data.map((project: Project) => {
+          if (project.flags.includes('wip')) {
+            wip = project
+          }
+          return (
+          (project.display && !project.flags.includes('wip')) &&
           <ProjectItem key={project.id} project={project} />
-        ))}
+        )})}
+        <h1 className="text-gray-400 font-sans text-2xl mb-4">What's Next?</h1>
+        <ProjectItem project={wip} />
       </ul>
+
     </div>
   )
 }
